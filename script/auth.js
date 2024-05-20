f6ec1bea6566b58284861c8649af2db5935ec1d7
Chu-rill <churchillrimamskep@gmail.com>
2024-05-20 20:09:38 +0100

    changed

---
+++ script/auth.js
@@ -0,0 +1,7 @@
+let button = document.querySelector("button");
+console.log(button);
+
+button.addEventListener("click", () => {
+  window.location.href = "/pages/home.html";
+  console.log(button);
+});
